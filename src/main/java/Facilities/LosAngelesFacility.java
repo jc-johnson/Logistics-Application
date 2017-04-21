43bf6fe08383d52c3edcb5314358942780d792d7
Jordan <jjohnson@myself.com>
2017-04-21 01:14:14 -0700

    Working impl of Shortest Path; still a minor bug to figure out

--- src/main/java/Facilities/LosAngelesFacility.java
+++ src/main/java/Facilities/LosAngelesFacility.java
@@ -13,6 +13,41 @@ public class LosAngelesFacility implements Facility {
 
 
     @Override
+    public String getLocation() {
+        return null;
+    }
+
+    @Override
+    public String setLocation(String location) {
+        return null;
+    }
+
+    @Override
+    public Long getRatePerDay() {
+        return null;
+    }
+
+    @Override
+    public Long setRatePerDay(String ratePerDay) {
+        return null;
+    }
+
+    @Override
+    public Long getCostPerDay() {
+        return null;
+    }
+
+    @Override
+    public Long setCostPerDay(String ratePerday) {
+        return null;
+    }
+
+    @Override
+    public void addNeighbor(String neighborLocation, String distance) {
+
+    }
+
+    @Override
     public void printOutput() {
 
     }
